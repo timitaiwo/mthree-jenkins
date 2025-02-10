7c617505f0d6a8279518b5af8e63366399425d1c
Timi Taiwo <31576028+timitaiwo@users.noreply.github.com>
2025-02-10 15:15:07 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Let's wander where there is no wifi!")
+    System.out.println("Let's wander where there is no wifi!");
   }
 }
